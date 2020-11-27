b0729239e9ea21ac7eb728eba57e68a581c44b0d
Dmitry O. Markov <markov.dm@inbox.ru>
2020-11-27 19:49:04 +0300

    intermediate stage for testing view users/show

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -3,47 +3,65 @@ require 'rails_helper'
 # Тест на шаблон users/show.html.erb
 
 RSpec.describe 'users/show', type: :view do
-  # Перед каждым шагом мы пропишем в переменную @users пару пользователей
-  # как бы имитируя действие контроллера, который эти данные будет брать из базы
-  # Обратите внимание, что мы объекты в базу не кладем, т.к. пишем FactoryBot.build_stubbed
+  context 'user = current_user' do
+    before(:each) do
+      user = assign(:user, FactoryBot.build_stubbed(
+        :user,
+        id: 10,
+        name: 'Вадик',
+        balance: 5000
+        )
+      )
+      allow(view).to receive(:current_user) { user }
 
-  before(:each) do
-    user = assign(:user, FactoryBot.build_stubbed(:user, id: 1, name: 'Вадик', balance: 5000))
-    allow(view).to receive(:current_user) { user }
-    # @user = users[0]
-    # game = assigns(:game)
-    render
-  end
-  # Проверяем, что шаблон выводит имена игроков
+      stub_template("users/_game.html.erb" => "User game goes here")
 
-  it 'renders player names' do
-    expect(rendered).to match 'Вадик'
-    # expect(rendered).to match 'Миша'
-  end
+      render
+    end
 
-  it 'checks that other user cant see link to change pass' do
-    user = assign(:user, FactoryBot.build_stubbed(:user, id: 2, name: 'Миша', balance: 35000))
-    allow(view).to receive(:current_user) { user }
-    expect(rendered).to match 'Сменить имя и пароль'
-    # before do
-    #   allow(view).to receive(:admin?).and_return(true)
-    # end
-    # current_user = user[0]
-    # expect(rendered).not_to match 'Вадик'
-    # response.body.match have_no_content("Hello world")
-    # expect(rendered).to match 'Миша'
+    it 'renders player names' do
+      expect(rendered).to match 'Вадик'
+    end
+
+    it 'user can see link to change pass' do
+      expect(rendered).to have_link('Сменить имя и пароль', href: '/users/edit.10')
+    end
+
+    it 'shows games list' do
+      expect(rendered).to match 'User game goes here'
+    end
   end
 
-  # # Проверяем, что шаблон выводит балансы игроков
-  # it 'renders player balances' do
-  #   expect(rendered).to match '5 000 ₽'
-  #   expect(rendered).to match '3 000 ₽'
-  # end
-  #
-  # # Проверяем, что шаблон выводит игроков в нужном порядке
-  # # (вообще говоря, тест избыточный, т.к. за порядок объектов в @users отвечает контроллер,
-  # # но чтобы показать, как тестировать порядок элементов на странице, полезно)
-  # it 'renders player names in right order' do
-  #   expect(rendered).to match /Вадик.*Миша/m
-  # end
+  context 'user != current_user' do
+    before(:each) do
+      assign(:user, FactoryBot.build_stubbed(
+        :user,
+        id: 1,
+        name: 'Вадик',
+        balance: 5000
+        )
+      )
+
+      allow(view).to receive(:current_user) {
+        assign(
+          :user, FactoryBot.build_stubbed(
+            :user,
+            id: 2,
+            name: 'Михаил',
+            balance: 25000
+          )
+        )
+      }
+
+      render
+    end
+
+    it 'renders player names' do
+      expect(rendered).to match 'Вадик'
+    end
+
+    it 'checks that other user cant see link to change pass' do
+      expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+  end
 end

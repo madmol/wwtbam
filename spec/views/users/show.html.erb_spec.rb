929779e5c98f62a1dbd1dd1e92e696cb5b5df9e5
Dmitry O. Markov <markov.dm@inbox.ru>
2020-11-28 02:25:39 +0300

    updtaed tests for view Users#show using devise helper for sign_in

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,70 +3,29 @@ require 'rails_helper'
 # Тест на шаблон users/show.html.erb
 
 RSpec.describe 'users/show', type: :view do
-  context 'user = current_user' do
-    before(:each) do
-      user = assign(:user, FactoryBot.build_stubbed(
-        :user,
-        id: 10,
-        name: 'Вадик',
-        balance: 5000
-        )
-      )
-      allow(view).to receive(:current_user) { user }
+  let(:user) { FactoryBot.create(:user, name: 'Вадик', id: 10) }
 
-      assign(:games, [
-        double(:name => "First game"),
-        double(:name => "Second game")
-      ])
+  before(:each) do
+    assign(:user, user)
 
-      stub_template("users/_game.html.erb" => "<%= game.name %>")
+    assign(:games, [
+      double(name: "First game"),
+      double(name: "Second game")
+    ])
 
-      render
-    end
+    stub_template("users/_game.html.erb" => "<%= game.name %>")
+  end
+
+  context 'user != current user' do
+    before(:each) { render }
 
     it 'renders player names' do
       expect(rendered).to match 'Вадик'
     end
 
-    it 'users can see link to change pass' do
-      expect(rendered).to have_link('Сменить имя и пароль', href: '/users/edit.10')
-    end
-
     it "shows user's games list" do
       expect(rendered).to match /First game/
       expect(rendered).to match /Second game/
-    end
-  end
-
-  context 'user != current_user' do
-    before(:each) do
-      assign(:user, FactoryBot.build_stubbed(
-        :user,
-        id: 1,
-        name: 'Вадик',
-        balance: 5000
-        )
-      )
-
-      allow(view).to receive(:current_user) {
-        assign(
-          :user, FactoryBot.build_stubbed(
-            :user,
-            id: 2,
-            name: 'Михаил',
-            balance: 25000
-          )
-        )
-      }
-
-      assign(:games, [
-        double(:name => "First game"),
-        double(:name => "Second game")
-      ])
-
-      stub_template("users/_game.html.erb" => "<%= game.name %>")
-
-      render
     end
 
     it 'renders player names' do
@@ -76,10 +35,16 @@ RSpec.describe 'users/show', type: :view do
     it 'checks that other users cant see link to change pass' do
       expect(rendered).not_to match 'Сменить имя и пароль'
     end
+  end
 
-    it "shows user's games list" do
-      expect(rendered).to match /First game/
-      expect(rendered).to match /Second game/
+  context 'user == current_user' do
+    before(:each) do
+      sign_in user
+      render
+    end
+
+    it 'users can see link to change pass' do
+      expect(rendered).to have_link('Сменить имя и пароль', href: '/users/edit.10')
     end
   end
 end
